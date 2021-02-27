99e1ce98ed5f7ca5fc5e760eaf5d6001e244f0bf
Art66066 <artur_martirosyan@edu.aua.am>
2021-02-27 23:24:04 +0400

    delTaskfontawesome

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,11 +1,28 @@
-import styles from './task.module.css'
+import styles from "./task.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { Form, Button } from "react-bootstrap";
 
 const Task = function (props) {
-    return (
-        <div className={styles.tasks}>
-            <p>{props.key1+1 + ".    " + props.task}</p>                
-        </div>
-    )
-}
+  const handleDelete = (e) => {
+    props.handleDeleteTask(props.task._id);
+  };
+  return (
+    <div className={styles.tasks}>
+      <div className={styles.div}>
+        <p>{props.key1 + 1 + ".    " + props.task.title}</p>
+        <Form.Check type="checkbox" />
+      </div>
+      <Button
+        size="sm"
+        variant="danger"
+        className={styles.btn}
+        onClick={handleDelete}
+      >
+        <FontAwesomeIcon icon={faTrash} />
+      </Button>
+    </div>
+  );
+};
 
-export default Task
+export default Task;
--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -1,45 +1,57 @@
-import React, { Component } from 'react'
-import AddTask from '../AddTask/AddTask';
+import React, { Component } from "react";
+import AddTask from "../AddTask/AddTask";
 import Task from "../Task/Task";
-import styles from './todo.module.css';
-import {Container, Row, Col} from 'react-bootstrap'
-
+import styles from "./todo.module.css";
+import { Container, Row, Col } from "react-bootstrap";
+import idGenerator from "../../helpers/idGenerator";
 
 export default class ToDo extends Component {
-    state = {
-        tasks: []
-    }
-    handleSubmit = (value) => {
-        const tasks = [...this.state.tasks];
-        tasks.push(value);
-        this.setState({
-            tasks
-        });
-    }
+  state = {
+    tasks: [],
+  };
+  handleSubmit = (value) => {
+    const tasks = [...this.state.tasks];
+    tasks.push({ title: value, _id: idGenerator() });
+    this.setState({
+      tasks,
+    });
+  };
 
-    render() {
-        const tasksJSX = this.state.tasks.map(function (task, index) {
-            return <Col key={index} xs={12} sm={6} md={4} lg={3} >
-                    <Task task={task} key1={index}/> 
-                </Col>});
-        return (
-            <Container fluid={true}>
-                <Row>
-                    <Col>
-                    <h1 className={styles.h1}>My ToDos</h1>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <AddTask handleSubmit={this.handleSubmit} />
-                    </Col>
-                </Row>
-                <Row>
-                    {tasksJSX}
-                </Row>
-            </Container>
-        )
-    }
+  handleDeleteTask = (_id) => {
+    this.setState({
+      tasks: this.state.tasks.filter((item) => {
+        return item._id !== _id;
+      }),
+    });
+  };
+
+  render() {
+    const tasksJSX = this.state.tasks.map((task, index) => {
+      return (
+        <Col key={task._id} xs={12} sm={6} md={4} lg={4}>
+          <Task
+            task={task}
+            key1={index}
+            handleDeleteTask={this.handleDeleteTask}
+          />
+        </Col>
+      );
+    });
+
+    return (
+      <Container fluid={true}>
+        <Row>
+          <Col>
+            <h1 className={styles.h1}>My ToDos</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <AddTask handleSubmit={this.handleSubmit} />
+          </Col>
+        </Row>
+        <Row>{tasksJSX}</Row>
+      </Container>
+    );
+  }
 }
-
-
